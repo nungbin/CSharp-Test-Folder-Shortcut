46c50274be3fd2579c11fd8dc71a58c537fdcdb8
nungbin <72701228+nungbin@users.noreply.github.com>
2022-05-27 10:37:47 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// ./CSharp_Test_Shortcut -Folder=c:\bin\p.lnk
+using ShellLink;
+
+namespace TestShortcut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://github.com/CaledoniaProject/LNK-Parser-csharp/blob/master/ConsoleApp2/Program.cs            
+            Shortcut shortcuts;
+            string controlFileName = "";
+
+            try {
+                // Examine command line parameters
+                foreach (string arg in args)
+                {
+                    if (arg.ToUpper().StartsWith("-FOLDER="))
+                    {
+                        controlFileName = arg.Split('=')[1];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error has occurred during Execution" + ex.Message);
+            }
+
+            try {
+                //shortcuts = Shortcut.ReadFromFile(@"C:\Bin\p.lnk");
+                shortcuts = Shortcut.ReadFromFile(controlFileName);
+
+                Console.WriteLine(
+                    //Shortcut.ReadFromFile(@"C:\Bin\calc.lnk")
+                    //Shortcut.ReadFromFile(@"C:\Bin\p.lnk")
+                    shortcuts.LinkInfo.LocalBasePath
+                );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error has occurred during Execution" + ex.Message);
+            }
+        }
+    }
+}
